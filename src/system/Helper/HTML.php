ecf732dec49cd31cd49b81d41236ffaa0d1ba7c5
Nguyen The Thuc <thucfami@gmail.com>
2018-10-18 12:32:37 +0700

    combine

--- src/system/Helper/HTML.php
+++ src/system/Helper/HTML.php
@@ -30,25 +30,25 @@ class HTML {
             echo "Router default not config";
             exit;
         }
-        
+
         if (!isset($sysconfig['routerDefault']['module'])) {
             echo "Router default module not config";
             exit;
         }
-        
-        
+
+
         if (!isset($sysconfig['routerDefault']['controller'])) {
             echo "Router default controller not config";
             exit;
         }
-        
-        
+
+
         if (!isset($sysconfig['routerDefault']['action'])) {
             echo "Router default action not config";
             exit;
         }
-        
-     
+
+
 
         $detach = explode("/", $path_uri);
 
@@ -95,4 +95,37 @@ class HTML {
 
         return $router;
     }
+
+    /**
+     * 
+     *   $files = array(
+     *       'http://myframework.com/static/plugins/core/pace/pace.min.js',
+     *       'http://myframework.com/static/js/libs/jquery-2.1.1.min.js',
+     *       'http://myframework.com/static/js/libs/jquery-ui-1.10.4.min.js',
+     *   );
+     *
+     *  echo \system\Helper\HTML::combine($files, 'minified_files/', md5("my_mini_file") . ".js");
+     */
+    public static function combine($array_files, $destination_dir, $dest_file_name) {
+
+        if (!is_file($destination_dir . $dest_file_name)) { //continue only if file doesn't exist
+            $content = "";
+            foreach ($array_files as $file) { //loop through array list
+                $content .= file_get_contents($file); //read each file
+            }
+
+            //You can use some sort of minifier here 
+            //minify_my_js($content);
+
+            $new_file = fopen($destination_dir . $dest_file_name, "w"); //open file for writing
+            fwrite($new_file, $content); //write to destination
+            fclose($new_file);
+
+            return '<script src="' . $destination_dir . $dest_file_name . '"></script>'; //output combined file
+        } else {
+            //use stored file
+            return '<script src="' . $destination_dir . $dest_file_name . '"></script>'; //output combine file
+        }
+    }
+
 }
