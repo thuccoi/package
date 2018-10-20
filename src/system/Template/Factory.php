6c049d7ee601958095960c6faa8738674cb72550
Nguyen The Thuc <thucfami@gmail.com>
2018-10-21 02:01:03 +0700

    connect database

--- src/system/Template/Factory.php
+++ src/system/Template/Factory.php
@@ -2,7 +2,7 @@
 
 namespace system\Template;
 
-use Doctrine\MongoDB\Connection;
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Doctrine\ODM\MongoDB\Configuration;
 use Doctrine\ODM\MongoDB\DocumentManager;
 use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
@@ -10,7 +10,7 @@ use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
 final class Factory implements \system\Template\ControllerInterface {
 
     public function __invoke($loader, $requestName, \system\Router $router, \system\Helper\Code $code, array $config = null, array $options = null) {
-        
+
         //connect database
         $dm = $this->connectDm($loader, $config);
 
@@ -18,82 +18,72 @@ final class Factory implements \system\Template\ControllerInterface {
     }
 
     public function connectDm($loader, $config) {
-
-        //get config database
         if (!isset($config['doctrine_mongo'])) {
-            throw new \RuntimeException('Not find config doctrine_mongo in system config');
+            echo "Not found config doctrine_mongo in system config";
+            exit;
         }
 
-        $config_dm = $config['doctrine_mongo'];
-
-        //set map namespace
-        foreach ($config_dm['map'] as $key => $val) {
-            //add namespace
-            $loader->add($key, $val);
+        if (!isset($config['doctrine_mongo']['connect'])) {
+            echo "Not found config connect doctrine_mongo in system config";
+            exit;
         }
 
-        if (!isset($config_dm['connect'])) {
-            throw new \RuntimeException('Not find config connect in doctrine_mongo');
+        if (!isset($config['doctrine_mongo']['connect']['host'])) {
+            echo "Not found config host connect doctrine_mongo in system config";
+            exit;
         }
 
-        if (!isset($config_dm['connect']['host'])) {
-            throw new \RuntimeException('Not find config host connect in doctrine_mongo');
+        if (!isset($config['doctrine_mongo']['connect']['port'])) {
+            echo "Not found config port connect doctrine_mongo in system config";
+            exit;
         }
 
-        if (!isset($config_dm['connect']['port'])) {
-            throw new \RuntimeException('Not find config port connect in doctrine_mongo');
-        }
+        //new config
+        $config_dm_system = $config['doctrine_mongo'];
 
-        $stringconnect = $config_dm['connect']['host'] . ":" . $config_dm['connect']['port'];
-        //setconfig connect
-        if (isset($config_dm['connect']['username']) && $config_dm['connect']['username']) {
+        $uri = 'mongodb://' . $config['doctrine_mongo']['connect']['host'] . ':' . $config['doctrine_mongo']['connect']['port'];
 
-            if (!isset($config_dm['connect']['password'])) {
-                throw new \RuntimeException('Not find config password connect in doctrine_mongo');
+        if (isset($config['doctrine_mongo']['connect']['username']) && $config['doctrine_mongo']['connect']['username']) {
+            if (!isset($config['doctrine_mongo']['connect']['password'])) {
+                echo "Not found config password connect doctrine_mongo in system config";
+                exit;
             }
 
-            $stringconnect = $config_dm['connect']['username'] . ":" . $config_dm['connect']['password'] . "@" . $stringconnect;
+            if (!isset($config['doctrine_mongo']['connect']['db_oauth'])) {
+                echo "Not found config db_oauth connect doctrine_mongo in system config";
+                exit;
+            }
+
+            $uri = 'mongodb://' . $config['doctrine_mongo']['connect']['username'] . ':' . $config['doctrine_mongo']['connect']['password'] . '@' . $config['doctrine_mongo']['connect']['host'] . ':' . $config['doctrine_mongo']['connect']['port'] . '/' . $config['doctrine_mongo']['connect']['db_oauth'];
         }
+
+        $client = new \MongoDB\Client($uri, [], [
+            'typeMap' => [
+                'root' => 'array',
+                'document' => 'array',
+                'array' => 'array',
+            ],
+        ]);
+
+
+        AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
         //connect database
-        $connection = new Connection($stringconnect);
-
         $configdm = new Configuration();
-
-        if (!isset($config_dm['data']['Proxies'])) {
-            throw new \RuntimeException('Not find config dir Proxies data in doctrine_mongo');
-        }
-
-        //set data proxies and hydrators
-        $configdm->setProxyDir($config_dm['data']['Proxies']);
+        $configdm->setProxyDir($config_dm_system['data']['Proxies']);
         $configdm->setProxyNamespace('Proxies');
-
-
-        if (!isset($config_dm['data']['Hydrators'])) {
-            throw new \RuntimeException('Not find config dir Hydrators data in doctrine_mongo');
-        }
-
-
-        $configdm->setHydratorDir($config_dm['data']['Hydrators']);
+        $configdm->setHydratorDir($config_dm_system['data']['Hydrators']);
         $configdm->setHydratorNamespace('Hydrators');
 
-
-        if (!isset($config_dm['connect']['db_default'])) {
-            throw new \RuntimeException('Not find config db_default connect in doctrine_mongo');
-        }
-
-        //set db default
-        $configdm->setDefaultDB($config_dm['connect']['db_default']);
+        $configdm->setDefaultDB($config_dm_system['connect']['db_default']);
 
         //meta annotation driver
-        foreach ($config_dm['map'] as $val) {
+        foreach ($config_dm_system['map'] as $val) {
             $configdm->setMetadataDriverImpl(AnnotationDriver::create($val));
         }
 
-        AnnotationDriver::registerAnnotationClasses();
+        $dm = DocumentManager::create($client, $configdm);
 
-        $dm = DocumentManager::create($connection, $configdm);
-        
         return $dm;
     }
 
