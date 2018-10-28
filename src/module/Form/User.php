0f5e60832b8061ac3bcee244ac3fafa869657b74
Nguyen The Thuc <thucfami@gmail.com>
2018-10-29 00:38:26 +0700

    create new an User

--- src/module/Form/User.php
+++ src/module/Form/User.php
@@ -16,28 +16,49 @@ class User {
     public function create($data) {
 
         //field required
+        //first name
         if (\system\Helper\Validate::isEmpty($data->first_name)) {
             $this->code->forbidden("first_name is require");
         }
 
+        if (!\system\Helper\Validate::isString($data->first_name)) {
+            $this->code->forbidden("first_name was not string");
+        }
+
+        //last name
         if (\system\Helper\Validate::isEmpty($data->last_name)) {
             $this->code->forbidden("last_name is require");
         }
 
+        if (!\system\Helper\Validate::isString($data->last_name)) {
+            $this->code->forbidden("last_name was not string");
+        }
+
+        //username
         if (\system\Helper\Validate::isEmpty($data->username)) {
             $this->code->forbidden("username is require");
         }
 
+        if (!\system\Helper\Validate::isString($data->username)) {
+            $this->code->forbidden("username was not string");
+        }
+
+        //password
         if (\system\Helper\Validate::isEmpty($data->password)) {
             $this->code->forbidden("password is require");
         }
 
+        if (!\system\Helper\Validate::isString($data->password)) {
+            $this->code->forbidden("password was not string");
+        }
+        
+        //email
         if (\system\Helper\Validate::isEmpty($data->email)) {
             $this->code->forbidden("email is require");
         }
 
         if (!\system\Helper\Validate::isEmail($data->email)) {
-            $this->code->notfound("email is not valid");
+            $this->code->notfound("email was not valid email");
         }
 
         try {
@@ -51,7 +72,13 @@ class User {
                     ->setLastName($data->last_name)
                     ->setEmail($data->email);
 
+            //isset phone
             if (!\system\Helper\Validate::isEmpty($data->phone)) {
+                //check phone is string
+                if (!\system\Helper\Validate::isString($data->phone)) {
+                    $this->code->notfound("phone was not string");
+                }
+
                 $user->setPhone($data->phone);
             }
 
