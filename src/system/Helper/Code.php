8f11442795c59f38509d0dd772101c7c5d39162f
Nguyen The Thuc <thucfami@gmail.com>
2018-10-29 00:12:53 +0700

    form

--- src/system/Helper/Code.php
+++ src/system/Helper/Code.php
@@ -123,6 +123,7 @@ class Code {
 
     //release ajax
     public function release($status = 405, $message = "Error!", $data = []) {
+        header('Content-Type: application/json');
         echo json_encode([
             "status" => $status,
             "message" => $message,
