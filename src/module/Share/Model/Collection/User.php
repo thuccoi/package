180901b79a183e931599dae1597ebc4d562154ee
Nguyen The Thuc <thucfami@gmail.com>
2018-10-29 23:08:48 +0700

    url

--- src/module/Share/Model/Collection/User.php
+++ src/module/Share/Model/Collection/User.php
@@ -91,7 +91,7 @@ class User extends \module\Share\Model\Common\AbsField {
 
         $mail->subject("Bạn đã tạo tài khoản");
 
-        $body = $mail->inline($config['ROOT_URL'] . 'a/notify/register', ['http://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700', DIR_ROOT . 'public/tami/css/tami.css']);
+        $body = $mail->inline($config['ROOT_URL'] . '/a/notify/register', ['http://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700', DIR_ROOT . 'public/tami/css/tami.css']);
 
         $mail->body($body);
 
