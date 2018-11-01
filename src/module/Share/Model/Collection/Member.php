15736681ff5c322f77ead18d8977573e8a5ebec5
Nguyen The Thuc <thucfami@gmail.com>
2018-11-02 00:12:57 +0700

    member id

--- src/module/Share/Model/Collection/Member.php
+++ src/module/Share/Model/Collection/Member.php
@@ -95,7 +95,7 @@ class Member extends \module\Share\Model\Common\AbsField {
         $mail->subject("Kích hoạt thành viên mới trong ứng dụng của bạn");
 
         //get html inline to body
-        $body = $mail->inline($config['URL_ROOT'] . "/a/notify/member-activate?id={$member->geetId()}&token={$member->getToken()}", ['http://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700', $config['DIR_PUBLIC'] . 'tami/css/tami.css', $config['DIR_PUBLIC'] . "css/account/notify/member/activate.css"]);
+        $body = $mail->inline($config['URL_ROOT'] . "/a/notify/member-activate?id={$member->getId()}&token={$member->getToken()}", ['http://fonts.googleapis.com/css?family=Quattrocento+Sans:400,700', $config['DIR_PUBLIC'] . 'tami/css/tami.css', $config['DIR_PUBLIC'] . "css/account/notify/member/activate.css"]);
 
         $mail->body($body);
 
