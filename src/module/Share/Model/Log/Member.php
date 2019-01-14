9f7dc7aa485d4ef13cc7b3a92c442e7da7e68dab
Nguyen The Thuc <thucfami@gmail.com>
2019-01-14 19:20:33 +0700

    connect

--- src/module/Share/Model/Log/Member.php
+++ src/module/Share/Model/Log/Member.php
@@ -4,6 +4,9 @@ namespace module\Share\Model\Log;
 
 class Member extends \module\Share\Model\Common\AbsLog {
 
+    //entity default
+    use \module\Share\Model\Common\EntityDefault;
+    
     public function __construct($connect, \system\Helper\Code $code, $config) {
         static::$parents = [\module\Share\Model\Log\App::class, \module\Share\Model\Log\User::class];
         $this->init($connect, $code, $config);
