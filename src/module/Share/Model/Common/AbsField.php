81cfb02b057dcacf708aa3e00a4fa3cb23bbae8d
Nguyen The Thuc <thucfami@gmail.com>
2019-02-11 19:43:37 +0700

    cache

--- src/module/Share/Model/Common/AbsField.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace module\Share\Model\Common;
-
-abstract class AbsField {
-
-    //release function
-    abstract public function release();
-}
--- src/system/Template/ControllerInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace system\Template;
-
-interface ControllerInterface {
-
-    public function __invoke($loader, $requestName, \system\Router $router, \system\Helper\Code $code, \system\Session $session, array $config = null, array $options = null);
-}
