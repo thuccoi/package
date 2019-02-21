2f3e34d142c6f8328d64de779265f7c63481b6b3
Nguyen The Thuc <thucfami@gmail.com>
2019-02-21 16:13:41 +0700

    assignment role to member

--- src/module/Assignment/Controller/MemberController.php
+++ src/module/Assignment/Controller/MemberController.php
@@ -12,11 +12,12 @@ class MemberController extends \system\Template\AbstractController {
 
         //init entity
         $this->entity = new \module\Assignment\Model\Entity\Role($connect, $code, $config);
+        $this->entity_member = new \module\Share\Model\Link\Member($connect, $code, $config);
     }
 
     public function indexAction() {
-        
-        
+
+
         $roles = $this->getConnect()->createQueryBuilder(\module\Assignment\Model\Collection\Role::class)
                 ->field('app_id')->equals($this->getViewer()->app->id)
                 ->getQuery()
@@ -36,6 +37,76 @@ class MemberController extends \system\Template\AbstractController {
             "roles"   => $roles,
             "members" => $members
         ];
+    }
+
+    public function hasRoleAction() {
+
+        $memid = $this->getCode()->post('member_id');
+        $roleid = $this->getCode()->post('role_id');
+
+        $count = $this->getConnect()->createQueryBuilder(\module\Assignment\Model\Collection\RoleToMember::class)
+                ->field('member.id')->equals($memid)
+                ->field('role.id')->equals($roleid)
+                ->field('app_id')->equals($this->getViewer()->app->id)
+                ->count()
+                ->getQuery()
+                ->execute();
+
+        $this->getCode()->success("ok", ["status" => ($count > 0)]);
+    }
+
+    public function removeRoleAction() {
+
+        $memid = $this->getCode()->post('member_id');
+        $roleid = $this->getCode()->post('role_id');
+
+        $this->getConnect()->createQueryBuilder(\module\Assignment\Model\Collection\RoleToMember::class)
+                ->field('member.id')->equals($memid)
+                ->field('role.id')->equals($roleid)
+                ->field('app_id')->equals($this->getViewer()->app->id)
+                ->remove()
+                ->getQuery()
+                ->execute();
+
+        $this->getCode()->success("ok");
+    }
+
+    public function addRoleAction() {
+
+        $memid = $this->getCode()->post('member_id');
+        $roleid = $this->getCode()->post('role_id');
+
+        $count = $this->getConnect()->createQueryBuilder(\module\Assignment\Model\Collection\RoleToMember::class)
+                ->field('member.id')->equals($memid)
+                ->field('role.id')->equals($roleid)
+                ->field('app_id')->equals($this->getViewer()->app->id)
+                ->count()
+                ->getQuery()
+                ->execute();
+
+        if ($count > 0) {
+            $this->getCode()->forbidden("Thành viên đã có vai trò này rồi");
+        }
+
+        $role = $this->entity->find($roleid);
+        if (!$role) {
+            $this->getCode()->notfound("Không tìm thấy vai trò này trong hệ thống");
+        }
+
+        $member = $this->entity_member->find($memid);
+        if (!$member) {
+            $this->getCode()->notfound("Không tìm thấy thành viên này trong hệ thống");
+        }
+
+        $obj = new \module\Assignment\Model\Collection\RoleToMember();
+        $obj->setAppId($this->getViewer()->app->id);
+        $obj->setMember($member);
+        $obj->setRole($role);
+
+        $this->getConnect()->persist($obj);
+        $this->getConnect()->flush();
+
+        $this->getCode()->success("ok");
     }
 
 }
